547f339dd93ef90b569324f09ac0d7552afa368d
kjy93913 <kjy_@naver.com>
2021-09-06 00:04:01 +0900

    mysql8

--- backeknd/app.js
+++ backeknd/app.js
@@ -11,24 +11,6 @@ var loginRouter = require('./routes/login');
 var app = express();
 
 
-var mysql = require('mysql');
-// Connection 객체 생성 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',   
-  password: 'skek93',
-  database: 'test_crud'  
-});  
-// Connect
-connection.connect(function (err) {   
-  if (err) {     
-    console.error('mysql connection error');     
-    console.error(err);     
-    throw err;   
-  } 
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
